b893986e319a28165a03a828526589566989206e
Marcus Vinícius da Silva Borges <marcus.f22@gmail.com>
2020-07-18 18:48:11 -0300

    refacts some parameters into instance variables

--- app/lib/zenvia/base_message_handler.rb
+++ app/lib/zenvia/base_message_handler.rb
@@ -3,16 +3,16 @@ module Zenvia
     def call(event:)
       return unless whatsapp_incoming_message_event?(event)
 
-      message_event = event.dig(:message)
-      contents = message_event.dig(:contents)
+      @message_event = event.dig(:message)
+      @contents = message_event.dig(:contents)
 
-      if delivery_address_message?(message_event)
+      if delivery_address_message?
         Zenvia::DeliveryAddressMessageHandler.new.call(
-          address_message: message_event[:contents].last,
-          phone_number: message_event[:from]
+          address_message: message_text,
+          phone_number: @message_event[:from]
         )
-      elsif ordering_request_message?(message_event)
-        Zenvia::OrderMessageHandler.new.call(message_text, message_event[:from])
+      elsif ordering_request_message?
+        Zenvia::OrderMessageHandler.new.call(message_text, @message_event[:from])
       end
     end
 
@@ -25,15 +25,14 @@ module Zenvia
     end
 
     def message_text
-      @message_text ||=
-        @payload[:message][:contents].find { |content| content[:type] == "text" }.dig(:text)
+      @message_text ||= @contents.find { |content| content[:type] == "text" }.dig(:text)
     end
 
-    def delivery_address_message?(message_event)
+    def delivery_address_message?
       false
     end
 
-    def ordering_request_message?(message_event)
+    def ordering_request_message?
       false
     end
   end
