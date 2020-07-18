3a6d6ce5d04e1bd503fad3e64ee4a7348279e355
Marcus Vinícius da Silva Borges <marcus.f22@gmail.com>
2020-07-18 16:42:01 -0300

    creates skeleton of zenvia webhook message handler

--- app/controllers/api/zenvia_controller.rb
+++ app/controllers/api/zenvia_controller.rb
@@ -1,6 +1,17 @@
 module Api
-  class Zenvia < Api::ApiController
+  class ZenviaController < Api::ApiController
     def message_received
+      if params[:direction] == 'IN'
+        message = params.dig(:message)
+
+        if message
+          Zenvia::DeliveryAddressMessageHandler.new.call(
+            address_message: message[:contents].last,
+            phone_number: message[:from]
+          )
+        end
+      end
+      head :ok
     end
   end
 end
