9788314a4a63dd04b4c6249188ae9146053f9a3f
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2020-07-18 16:01:56 -0300

    add logic to create CollectivePurchase when batch size is reached

--- app/models/product_batch.rb
+++ app/models/product_batch.rb
@@ -2,5 +2,18 @@ class ProductBatch
   include Mongoid::Document
 
   field :batch_size, type: Integer
-  field :current_batch_size, type: Integer
+  field :orders, type: Array
+
+  def add_order(new_order)
+    orders << new_order
+    byebug
+    if current_batch_size >= batch_size
+      CollectivePurchase.create!(orders: orders)
+    end
+  end
+
+  def current_batch_size
+    orders.sum(&:quantity)
+  end
+
 end
