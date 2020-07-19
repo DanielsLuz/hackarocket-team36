be3178af75386133d88aa333289dcd480a9bdc19
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2020-07-19 18:25:54 -0300

    move notification to Order callback

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,6 +11,8 @@ class Order
   belongs_to :collective_purchase, optional: true
   belongs_to :product_batch, optional: true
 
+  after_update :notify_update_status
+
   delegate :phone_number, to: :user
 
   def to_pagarme_object
@@ -32,5 +34,14 @@ class Order
       beans: 'feijão',
       meat: 'carne'
     }[product.to_sym]
+  end
+
+  def notify_update_status
+    if status == 'paid'
+      Zenvia::Api::SendWhatsappMessage.call(
+        user.phone_number,
+        "Obrigado pelo seu pedido! Quando tiver outro pedido ou qualquer dúvida, é só falar com a gente."
+      )
+    end
   end
 end
