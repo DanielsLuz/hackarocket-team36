f5e96674bad11098db9e351e0cefc7f709ba51e9
Marcus Vinícius da Silva Borges <marcus.f22@gmail.com>
2020-07-18 21:27:05 -0300

    moves api call to its own module

--- app/models/collective_purchase.rb
+++ app/models/collective_purchase.rb
@@ -5,7 +5,7 @@ class CollectivePurchase
 
   def alert_success_order
     orders.map(&:user).map(&:phone_number).each do |cellphone|
-      Zenvia::SendWhatsappMessage.call(
+      Zenvia::Api::SendWhatsappMessage.call(
         cellphone,
         "Seu pedido de atacado foi construido. Estamos te enviado o link para pagamento"
       )
